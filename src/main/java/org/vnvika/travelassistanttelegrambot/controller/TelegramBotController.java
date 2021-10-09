c33fdf85f03efeaa9ec1722804f0e4fe786a2cf9
vnvika <vnvika@gmail.com>
2021-10-09 16:31:53 +0300

    save city and places

--- src/main/java/org/vnvika/travelassistanttelegrambot/controller/TelegramBotController.java
+++ src/main/java/org/vnvika/travelassistanttelegrambot/controller/TelegramBotController.java
@@ -2,13 +2,25 @@ package org.vnvika.travelassistanttelegrambot.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.vnvika.travelassistanttelegrambot.service.Bot;
+import org.vnvika.travelassistanttelegrambot.dto.CityDto;
+import org.vnvika.travelassistanttelegrambot.mapper.CityMapper;
+import org.vnvika.travelassistanttelegrambot.model.City;
+import org.vnvika.travelassistanttelegrambot.service.CityService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class TelegramBotController {
-    private final Bot bot;
+    private final CityService cityService;
+
+    @PostMapping("/city")
+    public ResponseEntity<CityDto> save(@Valid @RequestBody CityDto cityDto) {
+        City city = cityService.save(cityDto);
+        return ResponseEntity.ok(CityMapper.INSTANCE.toDTO(city));
+    }
 
 }
